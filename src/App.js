322acc1054d530b9c7cd74ac9c28c8ff1ca31cf2
ashiq611 <72414858+ashiq611@users.noreply.github.com>
2023-02-09 21:48:24 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, React } from 'react';
 
 function App() {
+  const [counter,setCounter] = useState(0);
+ 
+
+  function increaseCounter(paylaod) {
+    setCounter(counter + paylaod);
+  }
+
+  function dicreaseCounter(paylaod) {
+    if (counter > 0) {
+      setCounter(counter - paylaod);
+
+    }
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="counter-app">
+        <h3>Counter By React</h3>
+        <p>The Counter is {counter}</p>
+        <button onClick={() => increaseCounter(1)}>+</button>
+        <button onClick={() => dicreaseCounter(1)}>-</button> <br/>
+      </div>
     </div>
   );
 }
